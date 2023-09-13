6a7b5a0dc94662180f82a4d3735d37a1f11784e4
Bobby Luly <bobbyluly@gmail.com>
2023-09-13 15:58:59 -0600

    Test: add tests for creating game objects with attributes

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -18,6 +18,26 @@ describe StatTracker do
     it 'exists' do
       expect(stat_tracker).to be_a(StatTracker)
     end
+
+    it 'has attributes' do
+      expect(stat_tracker.games).to be_an(Array)
+      expect(stat_tracker.teams).to be_an(Array)
+      expect(stat_tracker.game_teams).to be_an(Array)
+    end
+
+    it 'creates a game objects array with game attributes' do
+      expect(stat_tracker.games.first).to be_a(Game)
+      expect(stat_tracker.games.first.game_id).to eq("2012030221")
+      expect(stat_tracker.games.first.season).to eq("20122013")
+      expect(stat_tracker.games.first.type).to eq("Postseason")
+      expect(stat_tracker.games.first.date_time).to eq("5/16/13")
+      expect(stat_tracker.games.first.away_team_id).to eq("3")
+      expect(stat_tracker.games.first.home_team_id).to eq("6")
+      expect(stat_tracker.games.first.away_goals).to eq(2)
+      expect(stat_tracker.games.first.home_goals).to eq(3)
+      expect(stat_tracker.games.first.venue).to eq("Toyota Stadium")
+      expect(stat_tracker.games.first.venue_link).to eq("/api/v1/venues/null")
+    end
   end
 
   describe 'instance methods' do
